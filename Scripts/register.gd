70fd2ed4c366102ff47df1f9988078d99d725c9d
Ilona Lapka <ilona2000123@wp.pl>
2022-03-11 17:45:48 +0100

    Added register and login page

--- Scripts/register.gd
+++ Scripts/register.gd
@@ -4,3 +4,8 @@ extends Control
 func _ready():
 	print("Register scene")
 	pass
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://Scenes/main.tscn")
+	pass # Replace with function body.
